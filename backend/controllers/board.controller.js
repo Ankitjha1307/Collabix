a31a82ef599f38515d97d85968a217d532a1b3e2
Ankit Jha <mrankitjha007@gmail.com>
2026-03-06 21:31:54 +0530

    board controllers created

--- backend/controllers/board.controller.js
+++ backend/controllers/board.controller.js
@@ -41,7 +41,6 @@ const createBoard = asyncHandler(async (req, res) => {
     .json(new ApiResponse(201, board, "Board created successfully!"));
 });
 
-
 const getBoardsByWorkspace = asyncHandler(async (req, res) => {
   const { workspaceId } = req.params;
 
@@ -66,4 +65,82 @@ const getBoardsByWorkspace = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, boards, "Boards fetched successfully!"));
 });
 
-export { createBoard, getBoardsByWorkspace };
+const getBoardById = asyncHandler(async (req, res) => {
+  const { boardId } = req.params;
+  const board = await Board.findById(boardId);
+  if (!board) {
+    throw new ApiError(404, "Board not found");
+  }
+  const membership = await WorkspaceMember.findOne({
+    workspaceId: board.workspaceId,
+    userId: req.user._id
+  });
+  if (!membership) {
+    throw new ApiError(403, "Access denied to this board");
+  }
+  return res
+    .status(200)
+    .json(new ApiResponse(200, board, "Board by ID fetched successfully!"));
+});
+
+const updateBoard = asyncHandler(async (req, res) => {
+  const { boardId } = req.params;
+  
+  const { name, description } = req.body;
+
+  const board = await Board.findById(boardId);
+  if (!board) {
+    throw new ApiError(404, "Board not found");
+  }
+
+  const membership = await WorkspaceMember.findOne({
+    workspaceId: board.workspaceId,
+    userId: req.user._id
+  });
+
+  if (!membership || !["OWNER", "ADMIN"].includes(membership.role)) {
+    throw new ApiError(403, "Only OWNER or ADMIN can update boards");
+  }
+
+  if (name && !name.trim()) {
+    throw new ApiError(400, "Board name cannot be empty");
+  }
+
+  if (name) {
+    board.name = name.trim();
+  }
+  if (description) {
+    board.description = description.trim();
+  }
+
+  const updatedBoard = await board.save();
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, updatedBoard, "Board updated successfully!"));
+});
+
+const deleteBoard = asyncHandler(async (req, res) => {
+  const { boardId } = req.params;
+
+  const board = await Board.findById(boardId);
+  if (!board) {
+    throw new ApiError(404, "Board not found");
+  }
+  const membership = await WorkspaceMember.findOne({
+    workspaceId: board.workspaceId,
+    userId: req.user._id
+});
+
+  if (!membership || !["OWNER", "ADMIN"].includes(membership.role)) {
+    throw new ApiError(403, "Only OWNER or ADMIN can delete boards");
+  }
+
+  await Board.findByIdAndDelete(boardId);
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, null, "Board deleted successfully!"));
+});
+
+export { createBoard, getBoardsByWorkspace, getBoardById, updateBoard, deleteBoard };
